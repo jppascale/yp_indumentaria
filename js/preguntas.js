50255d5ab341623eb627260df1edd98f1b14f944
germangarcialandaburu <germangarcialandaburu@gmail.com>
2023-10-24 19:35:36 -0300

    Se agrega preguntas html y modificaciones para su funcionamiento
    Se vincula cada html

---
+++ js/preguntas.js
@@ -0,0 +1,9 @@
+function toggleAnswer(question) {
+    const answer = question.nextElementSibling;
+
+    if (answer.style.display === 'block') {
+        answer.style.display = 'none';
+    } else {
+        answer.style.display = 'block';
+    }
+}
